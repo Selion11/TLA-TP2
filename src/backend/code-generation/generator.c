d0e5ee0ad5df097b9d217fe2f93a68169b505eb0
lucky_number <jfernandezdinardo@itba.edu.ar>
2023-11-18 15:26:58 -0300

    Add first basic genetarion code building
    * Currently, tests 02,09,10 & 11 works fine;
    * Not support for colors yet;
    * Not support for createNode UNION property yet;
    * parentNode param in some generation functions, no use yet.

--- src/backend/code-generation/generator.c
+++ src/backend/code-generation/generator.c
@@ -5,6 +5,101 @@
  * Implementación de "generator.h".
  */
 
-void Generator(int result) {
-	LogInfo("El resultado de la expresion computada es: '%d'.", result);
+FILE * file;
+
+void Generator(Program * result) {
+	LogDebug("[Generator] Program");
+
+	file = fopen("ast_graph.dot", "w+");
+	if(file == NULL){
+		LogError("Error opening file");
+		return;
+	}
+	if(result == NULL || result->statementList == NULL){
+		LogError("Compiling error: result, result->statementList -> NULL");
+		return;
+	}
+
+	fprintf(file, "digraph AST {\n");
+    
+    generateStatementList(result->statementList, "program");
+    
+    fprintf(file, "\n}");
+
+	fclose(file);
+}
+
+void generateStatementList(StatementList * statementList, const char * parentNode) {
+	LogDebug("[Generator] statementList");
+    Statement *statement = statementList->firstStatement;
+    
+		while (statement != NULL) {
+			if (statement->actionType == CREATION_AC) {
+				generateCreateNode(statement->createNode, parentNode);
+			} else if (statement->actionType == UNION_AC) {
+				generateConnectNodes(statement->connectNodes, parentNode);
+			}
+			
+			statement = statement->nextStatement;
+		}
+}
+
+void generateCreateNode(CreateNode * createNode, const char * parentNode) {
+	LogDebug("[Generator] createNode");
+    if (createNode != NULL) {
+        fprintf(file, "  %s [", createNode->name, createNode->name);
+        generateNodeProperties(createNode->nodeProperties, "createNode_%p", createNode);
+		fprintf(file, "];");
+    }
+}
+
+void generateNodeProperties(NodeProperties * nodeProperties, const char * parentNode, CreateNode * createNode) {
+	LogDebug("[Generator] nodeProperties");
+    if (nodeProperties != NULL) {
+        generatePropertyList(nodeProperties->propertyList, parentNode, createNode);
+    }
+}
+
+void generatePropertyList(PropertyList * propertyList, const char * parentNode, CreateNode * createNode) {
+	LogDebug("[Generator] propertyList");
+    Property * property = propertyList->firstProperty;
+    
+    while (property != NULL) {
+        if (property->propertyType == TEXT_PROP) {
+            fprintf(file, "label=%s, ",property->value.text);
+        } else if (property->propertyType == BACKGROUND_PROP) {
+            fprintf(file, "fillcolor=\"%s\", ",property->value.colorType);
+        } else if (property->propertyType == BORDER_PROP) {
+            fprintf(file, "color=\"%s\", ", (void *) property, property->value.colorType);
+        }
+        
+        property = property->nextProperty;
+    }
+}
+
+void generateConnectNodes(ConnectNodes * connectNodes, const char * parentNode) {
+	LogDebug("[Generator] connectNodes");
+    if (connectNodes != NULL) {
+		UnionType unionType = connectNodes->unionType;
+		if (unionType == ARROW_UNION || unionType == LOOP_UNION)
+		{
+			fprintf(file, " %s -> %s ", connectNodes->nodeFrom, connectNodes->nodeTo);
+		}
+		else if (unionType == DOUBLE_ARROW_UNION){
+			fprintf(file, " %s -> %s ", connectNodes->nodeFrom, connectNodes->nodeTo);
+			fprintf(file, " %s -> %s ", connectNodes->nodeTo, connectNodes->nodeFrom);
+		}
+        
+        generateLineType(connectNodes->lineType, "connectNodes_%p", connectNodes);
+    }
+}
+
+void generateLineType(LineType lineType, const char * parentNode, ConnectNodes * connectNodes) {
+	LogDebug("[Generator] lineType");
+	if (lineType == DOTTED_LINE){
+		fprintf(file, "[style=dotted]");
+	}
+	else if (lineType == STRONG_LINE){
+		fprintf(file, "[style=bold]");
+	}
 }
--- src/backend/code-generation/generator.h
+++ src/backend/code-generation/generator.h
@@ -1,6 +1,15 @@
 #ifndef GENERATOR_HEADER
 #define GENERATOR_HEADER
 
-void Generator(int result);
+#include "../../backend/semantic-analysis/abstract-syntax-tree.h"
+
+void Generator(Program * result);
+void generateStatementList(StatementList * statementList, const char * parentNode);
+void generateCreateNode(CreateNode * createNode, const char * parentNode);
+void generateNodeProperties(NodeProperties * nodeProperties, const char * parentNode, CreateNode * createNode);
+void generatePropertyList(PropertyList * propertyList, const char * parentNode, CreateNode * createNode);
+void generateConnectNodes(ConnectNodes * connectNodes, const char * parentNode);
+void generateLineType(LineType lineType, const char * parentNode, ConnectNodes * connectNodes);
+void generateUnionType(UnionType unionType, const char * parentNode, ConnectNodes * connectNodes);
 
 #endif
