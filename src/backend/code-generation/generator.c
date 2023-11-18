9dad9558cc42665d92c4c73835d29574f3468e5b
lucky_number <jfernandezdinardo@itba.edu.ar>
2023-11-18 20:05:42 -0300

    Add support for union property
    * Accept test 06 works properly
    * [BEHAVIOUR] Add stack for stack all the union
      properties and perform at the end of the programme

--- src/backend/code-generation/generator.c
+++ src/backend/code-generation/generator.c
@@ -5,10 +5,18 @@
  * Implementación de "generator.h".
  */
 
+//Global variables
 FILE * file;
+UnionWrappedNode * unionStack = NULL;
+int unionStackTop = -1;
+int unionStackSize = 0;
+int unionPropsFlag = 0;
 
 //Utility method
 const char* colorTypeToString(ColorType colorType);
+void initUnionStack(int size);
+void pushUnion(UnionWrappedNode unionNode);
+UnionWrappedNode popUnion(void);
 
 void Generator(Program * result) {
 	LogDebug("[Generator] Program");
@@ -23,13 +31,20 @@ void Generator(Program * result) {
 		return;
 	}
 
+	initUnionStack(10); 
+
 	fprintf(file, "digraph AST {\n");
     
     generateStatementList(result->statementList, "program");
     
-    fprintf(file, "\n}");
+	if(unionPropsFlag){
+		generatePopConnectNodes();
+	}
+
+    fprintf(file, "}");
 
 	fclose(file);
+	free(unionStack);
 }
 
 void generateStatementList(StatementList * statementList, const char * parentNode) {
@@ -73,7 +88,15 @@ void generatePropertyList(PropertyList * propertyList, const char * parentNode, 
         } else if (property->propertyType == BACKGROUND_PROP || property->propertyType == BORDER_PROP) {
             const char * colorString = colorTypeToString(property->value.colorType);
             fprintf(file, "%s=%s, ", (property->propertyType == BACKGROUND_PROP) ? "style=filled, fillcolor" : "color", colorString);
-        }
+        } else if (property->propertyType == UNION_PROP) {
+			UnionWrappedNode unionNode;
+			unionNode.nodeFromName = strdup(createNode->name);
+			unionNode.nodeToName = strdup(property->value.unionProp.nodeName);
+			unionNode.lineType = property->value.unionProp.lineType;
+
+			pushUnion(unionNode);
+			unionPropsFlag = 1;
+		}
         
         property = property->nextProperty;
     }
@@ -85,25 +108,37 @@ void generateConnectNodes(ConnectNodes * connectNodes, const char * parentNode) 
 		UnionType unionType = connectNodes->unionType;
 		if (unionType == ARROW_UNION || unionType == LOOP_UNION)
 		{
-			fprintf(file, " %s -> %s ", connectNodes->nodeFrom, connectNodes->nodeTo);
+			fprintf(file, "  %s -> %s ", connectNodes->nodeFrom, connectNodes->nodeTo);
 		}
 		else if (unionType == DOUBLE_ARROW_UNION){
-			fprintf(file, " %s -> %s ", connectNodes->nodeFrom, connectNodes->nodeTo);
-			fprintf(file, " %s -> %s ", connectNodes->nodeTo, connectNodes->nodeFrom);
+			fprintf(file, "  %s -> %s ", connectNodes->nodeFrom, connectNodes->nodeTo);
+			fprintf(file, "  %s -> %s ", connectNodes->nodeTo, connectNodes->nodeFrom);
 		}
         
-        generateLineType(connectNodes->lineType, "connectNodes_%p", connectNodes);
+        generateLineType(connectNodes->lineType);
     }
 }
 
-void generateLineType(LineType lineType, const char * parentNode, ConnectNodes * connectNodes) {
+void generateLineType(LineType lineType) {
 	LogDebug("[Generator] lineType");
 	if (lineType == DOTTED_LINE){
-		fprintf(file, "[style=dotted]");
+		fprintf(file, "[style=dotted];\n");
 	}
 	else if (lineType == STRONG_LINE){
-		fprintf(file, "[style=bold]");
+		fprintf(file, "[style=bold];\n");
+	} else {
+		fprintf(file, ";\n");
 	}
+}
+
+void generatePopConnectNodes() {
+    while (unionStackTop >= 0) {
+        UnionWrappedNode unionNode = popUnion();
+        fprintf(file, "  %s -> %s ", unionNode.nodeFromName, unionNode.nodeToName);
+        generateLineType(unionNode.lineType);
+    }
+
+	unionPropsFlag = 0;
 }
 
 /*********************************************************Utility Methods**************************************************************************/
@@ -122,5 +157,30 @@ const char* colorTypeToString(ColorType colorType) {
             return "violet";
         default:
             return "unknown_color";
+    }
+}
+
+void initUnionStack(int size) {
+    unionStack = (UnionWrappedNode *)malloc(size * sizeof(UnionWrappedNode));
+    unionStackSize = size;
+    unionStackTop = -1;
+}
+
+void pushUnion(UnionWrappedNode unionNode) {
+    if (unionStackTop < unionStackSize - 1) {
+        unionStack[++unionStackTop] = unionNode;
+    } else {
+        // Manejar error de desbordamiento de pila
+        LogError("Union stack overflow");
+    }
+}
+
+UnionWrappedNode popUnion(void) {
+    if (unionStackTop >= 0) {
+        return unionStack[unionStackTop--];
+    } else {
+        // Manejar error de pila vacía
+        LogError("Union stack underflow");
+        return (UnionWrappedNode){NULL, NULL, DOTTED_LINE}; // Valor de retorno de emergencia
     }
 }
--- src/backend/code-generation/generator.h
+++ src/backend/code-generation/generator.h
@@ -2,6 +2,15 @@
 #define GENERATOR_HEADER
 
 #include "../../backend/semantic-analysis/abstract-syntax-tree.h"
+#include <stdlib.h>
+#include <string.h>
+
+// Estructura para representar un elemento de la pila de uniones de nodos.
+typedef struct {
+    char *nodeFromName;
+    char *nodeToName;
+    LineType lineType;
+} UnionWrappedNode;
 
 void Generator(Program * result);
 void generateStatementList(StatementList * statementList, const char * parentNode);
@@ -9,7 +18,8 @@ void generateCreateNode(CreateNode * createNode, const char * parentNode);
 void generateNodeProperties(NodeProperties * nodeProperties, const char * parentNode, CreateNode * createNode);
 void generatePropertyList(PropertyList * propertyList, const char * parentNode, CreateNode * createNode);
 void generateConnectNodes(ConnectNodes * connectNodes, const char * parentNode);
-void generateLineType(LineType lineType, const char * parentNode, ConnectNodes * connectNodes);
+void generateLineType(LineType lineType);
 void generateUnionType(UnionType unionType, const char * parentNode, ConnectNodes * connectNodes);
+void generatePopConnectNodes();
 
 #endif
